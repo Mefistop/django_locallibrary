a900f8201d0276f87cad7235913d2c193f1601b5
Petr Baikov <petr.bajjkov@rambler.ru>
2024-10-31 20:53:02 +0300

    Feat: add session for count visits home  page

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -8,6 +8,8 @@ def index(request):
     """
     View function for the home page of the website.
     """
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
@@ -17,6 +19,7 @@ def index(request):
         'num_instances':num_instances,
         'num_instances_available': num_instances_available,
         'num_authors':num_authors,
+        'num_visits':num_visits,
     }
     return render(request, 'index.html', context=context)
 
